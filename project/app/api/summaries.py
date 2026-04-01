9501005012ffa695b3b6d84862ba69dfd2321b85
Gabriel Fortuny <92552031+gabrielfortuny@users.noreply.github.com>
2026-04-01 16:19:34 -0400

    summaries get endpoints and tests

--- project/app/api/summaries.py
+++ project/app/api/summaries.py
@@ -1,17 +1,28 @@
-from fastapi import APIRouter
+from typing import List
+
+from fastapi import APIRouter, HTTPException
 
 from app.models.pydantic import SummaryPayloadSchema, SummaryResponseSchema
-from app.models.tortoise import TextSummary
+from app.models.tortoise import SummarySchema, TextSummary
 
 router = APIRouter()
 
 
 @router.post("/", response_model=SummaryResponseSchema, status_code=201)
-async def create_summary(payload: SummaryPayloadSchema) -> SummaryResponseSchema:
-    summary = TextSummary(
-        url=payload.url,
-        summary="dummy summary",
-    )
-    await summary.save()
+async def create_summary(payload: SummaryPayloadSchema) -> TextSummary:
+    summary = await TextSummary.create(url=payload.url, summary="dummy summary")
+    return summary
 
-    return SummaryResponseSchema(id=summary.id, url=payload.url)  # pyright: ignore[reportAttributeAccessIssue]
+
+@router.get("/{id}/", response_model=SummarySchema)
+async def read_summary(id: int) -> SummarySchema:  # type: ignore[valid-type]
+    summary = await TextSummary.filter(id=id).first().values()
+    if not summary:
+        raise HTTPException(status_code=404, detail="Summary not found")
+    return summary
+
+
+@router.get("/", response_model=List[SummarySchema])
+async def read_all_summaries() -> List[SummarySchema]:  # type: ignore[valid-type]
+    summaries = await TextSummary.all().values()
+    return summaries
